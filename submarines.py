7e55215aca61d307997cbbc3b1149278d02f3d0d
doug <28678448+drm88@users.noreply.github.com>
2022-04-25 17:29:09 -0400

    added board logic

--- submarines.py
+++ submarines.py
@@ -1,4 +1,6 @@
 from random import randint
+import random
+import os
 
 
 class submarine:
@@ -20,6 +22,12 @@ class submarine:
              oriented = 'vertically.'
         return 'The submarine is postioned at ' + xycoord + ' and is oriented ' + oriented
 
+#Define the board, a 10x10 grid
+board = [[0] * 10 for i in range(10)]
+board_display = [["O"] * 10 for i in range(10)]
+
+#Functions
+#check if the submarine is in a valid position on the board, returns True if the position is valid.
 def check_position(x, y, horizontal):
     on_board = True
     if horizontal == True and int(x) > 7:
@@ -29,6 +37,13 @@ def check_position(x, y, horizontal):
         on_board = False
     return on_board
 
+#Prints the board
+def print_board(board_array):
+    print("\n  " + " ".join(str(i) for i in range(0, 10)))
+    for row in range(10):
+        print(str(row) + " " + " ".join(str(column) for column in board_array[row]))
+    print()
+
 
 #get the initial position and orientation of the player's submarine
 x = input('Enter X coordinate: ') #X coordinate
@@ -36,14 +51,24 @@ y = input('Enter Y coordinate: ') #Y coordinate
 horizontal = input('Is your submarine positioned horizontally? (y/n): ')
 orientation = str(horizontal).lower() in ("yes", "true", "t", "1", 'Y', 'y') #If true, orientation is horizontal, otherwise orientation is vertical
 
-valid_position = check_position(x, y, orientation)
+valid_position = check_position(x, y, orientation) #validates the selected position and orientation, if invalide prompts the user for new input
 if valid_position == False:
+    os.system('clear')
     print('This position is out of bounds. Please enter a new position or change orientation')
+    x = input('Enter X coordinate: ') #X coordinate
+    y = input('Enter Y coordinate: ') #Y coordinate
+    horizontal = input('Is your submarine positioned horizontally? (y/n): ')
+    orientation = str(horizontal).lower() in ("yes", "true", "t", "1", 'Y', 'y')
 else:
     player_sub = submarine(x, y, orientation)
 
-#intial conditions for the enemy submarine
-#enemy_sub = submarine(randint(0, 9), randint(0, 9), True)
+#intial conditions for the enemy submarine, randomly picks a valid position and orientation on the board
+enemy_orientation = random.choice([True, False])
+if enemy_orientation == True:
+    enemy_sub = submarine(randint(0, 7), randint(0, 9), enemy_orientation)
+else:
+    enemy_sub = submarine(randint(0, 9), randint(0, 7), enemy_orientation)
 
 #print(player_sub)
 #print(enemy_sub)
+print_board(board_display)
